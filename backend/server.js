c1dfc1737494f346e04b3c7f3d239a2bca1ac5fd
Damian <a.damiansanz@gmail.com>
2026-02-20 15:04:32 -0300

    Backend listo para producción

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient } = require("mongodb");
+
+const app = express();
+
+app.use(cors());
+
+const uri = process.env.MONGODB_URI;
+
+console.log("URI usada:", uri);
+
+const client = new MongoClient(uri);
+
+async function connectDB() {
+  try {
+    await client.connect();
+    console.log("Conectado sin SRV 🚀");
+  } catch (error) {
+    console.error("Error real:", error);
+  }
+}
+
+connectDB();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
