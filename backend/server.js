f13a1d634530a32f214b40f78bfb82b30efa2aa4
Damian <a.damiansanz@gmail.com>
2026-02-23 14:34:08 -0300

    server limpio y corregido

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,27 @@
-const Auto = require("./models/Auto");
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const Auto = require("./models/Auto");
 
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Conexión a MongoDB
 const uri = process.env.MONGO_URI;
 
 mongoose.connect(uri)
   .then(() => console.log("Conectado a MongoDB 🚀"))
   .catch(err => console.error("Error real:", err));
+
+// Rutas
 app.get("/", (req, res) => {
   res.send("Backend de Autos funcionando correctamente 🚀");
 });
 
-  app.get("/autos", async (req, res) => {
+app.get("/autos", async (req, res) => {
   try {
     const autos = await Auto.find();
     res.json(autos);
@@ -20,7 +29,6 @@ app.get("/", (req, res) => {
     res.status(500).json({ error: "Error al obtener autos" });
   }
 });
-app.use(express.json());
 
 app.post("/autos", async (req, res) => {
   try {
@@ -31,9 +39,10 @@ app.post("/autos", async (req, res) => {
     res.status(500).json({ error: "Error al guardar auto" });
   }
 });
+
+// Puerto
 const PORT = process.env.PORT || 3000;
 
-
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en puerto ${PORT} 🚀`);
 });
